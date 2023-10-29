42c0da22c9ad1d4089f7eefa13dcd3833d19fb94
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-10-29 10:25:16 +0530

    script_js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const questions=[
+    {
+        question:"Who was the first President of the United States?",
+        answers:
+        [
+            {value:"Joe Biden",correct:"false"},
+            {value:"George Washington",correct:"true"},
+            {value:"Donald Trump",correct:"false"},
+            {value:"Thomas Jefferson",correct:"false"},
 
+
+        ]
+
+        
+    },
+    {
+        question:"Which ancient wonder of the world was located in Egypt and is known for its colossal size?",
+        answers:
+        [
+            {value:"The Great Pyramid of Giza",correct:"true"},
+            {value:"Great Sphinx of Giza",correct:"false"},
+            {value:"Valley of the Kings",correct:"false"},
+            {value:"Karnak",correct:"false"},
+
+
+        ]
+
+        
+    },
+]
